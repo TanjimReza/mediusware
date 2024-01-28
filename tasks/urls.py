0c646ea1dc96c51a203cff551e849a459a51426d
Tanjim Reza <tanjimreza786@gmail.com>
2024-01-28 20:36:22 +0600

    Create Task done, Edit WIP

--- tasks/urls.py
+++ tasks/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('login/',views.user_login,name='login'),
     path('signup/',views.user_signup,name='register'),
     path('logout/',views.user_logout,name='logout'),
+    
+    path('create/',views.create_task,name='createtask'),
     # path('')
 ] 
--- tasks/views.py
+++ tasks/views.py
@@ -1,36 +1,40 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from . import views
 from django.urls import path, include
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
-from .models import User
+from .models import User, Task, TaskPhoto
 
 #! Usually I do this but not doing it for this project to keep the naming convention
 # from django.contrib.auth import login as auth_login
 # from django.contrib.auth import logout as auth_logout
 
 
-
-
-
 def index(request):
-    return render(request,'tasks/tasks_index.html')
-
+    if request.user.is_authenticated:
+        # Get tasks for the logged-in user
+        user_tasks = Task.objects.filter(user=request.user)
+        user_tasks = Task.objects.filter(
+            user=request.user).prefetch_related('photos')
+        return render(request, 'tasks/tasks_index.html', {'tasks': user_tasks})
+    else:
+        return redirect('login')
+    
 
 def user_login(request):
-    
+
     if request.method == 'GET':
-        return render(request,'tasks/user_login.html')
-    
+        return render(request, 'tasks/user_login.html')
+
     elif request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-        
-        user = authenticate(request,email=email,password=password)
+
+        user = authenticate(request, email=email, password=password)
         print(f"User: {user}")
         if user is not None:
             print(f"Logging in {user}")
-            login(request,user)
+            login(request, user)
             return redirect('index')
         else:
             print(f"User not found")
@@ -38,28 +42,62 @@ def user_login(request):
                 'error': 'Invalid Credentials'
             }
             return redirect('login')
-    
-    return render(request,'tasks/user_login.html')
+
+    return render(request, 'tasks/user_login.html')
+
 
 def user_signup(request):
-    
+
     if request.method == 'GET':
-        return render(request,'tasks/user_signup.html')
-    
+        return render(request, 'tasks/user_signup.html')
+
     elif request.method == 'POST':
         email = request.POST.get('email')
         full_name = request.POST.get('full_name')
         password = request.POST.get('password')
 
-        print(email,full_name,password)
-        user = User.objects.create_user(email=email,full_name=full_name,password=password)
+        print(email, full_name, password)
+        user = User.objects.create_user(
+            email=email, full_name=full_name, password=password)
         user.save()
-        
+
         print("User created")
-        
+
         return redirect('login')
-    
-    return render(request,'tasks/user_signup.html')
+
+    return render(request, 'tasks/user_signup.html')
+
 
 def user_logout(request):
-    return render(request,'tasks/user_logout.html')
+    return render(request, 'tasks/user_logout.html')
+
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        print(request.POST)
+        print(request.FILES)
+        
+        task = Task(title=title, description=description, due_date=due_date, user=request.user)
+        task.save()
+        
+        for file in request.FILES.getlist('photos'):
+            try:
+                TaskPhoto.objects.create(task=task, photo=file)
+            except Exception as e:
+                print(f"TaskImage Failed! Exception: {e}")
+        
+        return redirect('index')
+    return render(request, 'tasks/create_task.html')
+
+
+def delete_task_photo(request, photo_id):
+    # Ensure the user is authorized to delete the photo
+    photo = TaskPhoto.objects.get(id=photo_id)
+    if photo.task.user == request.user:  # Check ownership
+        photo.delete()
+        return HttpResponse('Photo deleted')
+    else:
+        return HttpResponse('Unauthorized', status=401)
